8346249a1f50861d34b171f4d442d238ec54970d
lukoki Makuntima Daniel <lukokimakuntimadaniel@gmail.com>
2023-05-07 14:53:23 +0100

    Adicionar arquivos de projeto.

---
+++ Problema_da_soma_reservada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problema_da_soma_reservada
+{
+    class Program
+    {
+        public static int inverter(int n)
+        {
+            string nString = n.ToString();
+            string nInvertida = "";
+            for(int cont=nString.Length-1;cont>=0; cont--)
+            {
+                nInvertida += nString.Substring(cont, 1);
+            }
+            return int.Parse(nInvertida);
+        }
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("" + inverter(inverter(n1) + inverter(n2)));
+        }
+    }
+}
